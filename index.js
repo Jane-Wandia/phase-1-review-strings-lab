2c95156089038d5a329956dac82279c068cbe437
janekigenyi <janekigenyi@gmail.com>
2022-08-22 15:28:01 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+
+let currentUser = 'Jane Kigenyi';
+let welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
+const upperCaseCurrentUser = currentUser.toUpperCase();
+let excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ' + upperCaseCurrentUser + '!';
+const firstInitial = currentUser[0];
+const restOfName = currentUser.slice(1);
+let shortGreeting = 'Welcome, ' + firstInitial + '!';
